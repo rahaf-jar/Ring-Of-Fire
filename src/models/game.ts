95b7b59252c1c2aa18f2feb012ef0de6afc353e5
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-10-15 16:56:06 +0200

    shuffle cards

--- src/models/game.ts
+++ src/models/game.ts
@@ -3,4 +3,30 @@ export class Game {
     public stack: string[] = [];
     public playedCards: string[] = [];
     public currentPlayer: number = 0;
+
+    constructor(){
+        for(let i=0; i < 14; i++){
+            this.stack.push('ace_' + i);
+            this.stack.push('clubs_' + i);
+            this.stack.push('diamonds_' + i);
+            this.stack.push('hearts_' + i);
+        }
+
+        shuffle(this.stack);
+    }
 }
+
+function shuffle(array: any[]) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex != 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        temporaryValue = array[currentIndex];
+        array[currentIndex], array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}   
