18a6c861e99491ce412acb0291cb1577dc22c905
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-10-16 14:15:56 +0200

    fixed all import errors

--- src/app/dialog-add-player/dialog-add-player.component.ts
+++ src/app/dialog-add-player/dialog-add-player.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatLabel } from '@angular/material/form-field';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+
+
 
 @Component({
   selector: 'app-dialog-add-player',
   standalone: true,
-  imports: [],
+  imports: [MatDialogModule, MatFormFieldModule, MatLabel, CommonModule, FormsModule, MatInputModule],
   templateUrl: './dialog-add-player.component.html',
   styleUrl: './dialog-add-player.component.scss'
 })
 export class DialogAddPlayerComponent {
+
+  name: string = '';
 
 }
