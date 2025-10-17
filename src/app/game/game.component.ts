4266652593cecc2b79b42c462f589009e0226d0b
Rahaf Jarrous <rahaf-jar@users.noreply.github.com>
2025-10-17 14:00:43 +0200

    players playing in turns

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -48,6 +48,8 @@ export class GameComponent implements OnInit {
       console.log('New Card: ' + this.currentCard);
       console.log('Game is: ' + this.game);
 
+      this.game.currentPlayer++;
+      this.game.currentPlayer %= this.game.players.length;
       setTimeout(() => {
         this.game.playedCards.push(this.currentCard);
         this.pickCardAnimation = false;
